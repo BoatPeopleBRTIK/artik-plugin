e068e9756834b8f67b5d8d3133ebcf53fd1eb5d3
Jaewon Kim <jaewon02.kim@samsung.com>
2016-04-21 21:11:01 +0900

    Update start_adbd.sh script for artik7
    Use absolute path insted of relative path in start_adbd.sh script
    
    Change-Id: Ia85b955042a9350347fd375c68cfe4505dccdf07
    Signed-off-by: Jaewon Kim <jaewon02.kim@samsung.com>

--- prebuilt/adbd/artik7/start_adbd.sh
+++ prebuilt/adbd/artik7/start_adbd.sh
@@ -1,35 +1,38 @@
+#!/bin/sh
+
+GADGET_DIR=/sys/kernel/config/usb_gadget/g1
+
 #Mount ConfigFS and create Gadget
-mount -t configfs none /sys/kernel/config
-mkdir /sys/kernel/config/usb_gadget/g1
-cd /sys/kernel/config/usb_gadget/g1
+mkdir ${GADGET_DIR}
 
 #Set default Vendor and Product IDs for now
-echo 0x18D1 > idVendor
-echo 0x0001 > idProduct
+echo 0x18D1 > ${GADGET_DIR}/idVendor
+echo 0x0001 > ${GADGET_DIR}/idProduct
 
 #Create English strings and add random deviceID
-mkdir strings/0x409
-echo 0123459876 > strings/0x409/serialnumber
+mkdir ${GADGET_DIR}/strings/0x409
+echo 0123456789 > ${GADGET_DIR}/strings/0x409/serialnumber
 
 #Update following if you want to
-echo "Samsung" > strings/0x409/manufacturer
-echo "Artik" > strings/0x409/product
+echo "Samsung" > ${GADGET_DIR}/strings/0x409/manufacturer
+echo "Artik" > ${GADGET_DIR}/strings/0x409/product
 
 #Create gadget configuration
-mkdir configs/c.1
-mkdir configs/c.1/strings/0x409
-echo "Conf 1" > configs/c.1/strings/0x409/configuration
-echo 120 > configs/c.1/MaxPower
+mkdir ${GADGET_DIR}/configs/c.1
+mkdir ${GADGET_DIR}/configs/c.1/strings/0x409
+echo "Conf 1" > ${GADGET_DIR}/configs/c.1/strings/0x409/configuration
+echo 120 > ${GADGET_DIR}/configs/c.1/MaxPower
 
 #Create Adb FunctionFS function
 #And link it to the gadget configuration
-killall adbd
-mkdir functions/ffs.adb
-ln -s /sys/kernel/config/usb_gadget/g1/functions/ffs.adb /sys/kernel/config/usb_gadget/g1/configs/c.1/ffs.adb
+mkdir ${GADGET_DIR}/functions/ffs.adb
+ln -s ${GADGET_DIR}/functions/ffs.adb ${GADGET_DIR}/configs/c.1/ffs.adb
 
-#Start adbd application
 mkdir -p /dev/usb-ffs/adb
 mount -o uid=2000,gid=2000 -t functionfs adb /dev/usb-ffs/adb
+
+#Start adbd application
 /usr/bin/adbd&
-sleep 1
-echo c0040000.dwc2otg > /sys/kernel/config/usb_gadget/g1/UDC
+echo $! > /run/adbd.pid
+sleep 0.2
+echo c0040000.dwc2otg > ${GADGET_DIR}/UDC
