886e982cd08afd21b8291ad070df4249d9928be3
Jaewon Kim <jaewon02.kim@samsung.com>
2016-05-18 19:18:14 +0900

    artik: adb: update adb start script
    This patch updates "start_adbd.sh" script to support RNDIS
    function and code cleanup.
    
    Change-Id: Ife8a5e88b66cb27a843aa095685db3640d8a993f
    Signed-off-by: Jaewon Kim <jaewon02.kim@samsung.com>

--- prebuilt/adbd/artik10/start_adbd.sh
+++ prebuilt/adbd/artik10/start_adbd.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
 
-echo adb > /sys/class/android_usb/android0/functions
-echo 1 > /sys/class/android_usb/android0/enable
+GADGET_DIR=/sys/class/android_usb/android0
+FUNCTIONS=adb
+
+if [ "`cat ${GADGET_DIR}/enable`" == "1" ]; then
+	echo 0 > ${GADGET_DIR}/enable
+fi
+
+if [ "`cat ${GADGET_DIR}/functions | grep rndis`" != "" ]; then
+	FUNCTIONS=${FUNCTIONS},rndis
+fi
+
+echo ${FUNCTIONS} > ${GADGET_DIR}/functions
+echo 1 > ${GADGET_DIR}/enable
 
 /usr/bin/adbd&
 echo $! > /run/adbd.pid
