e9adea1ab83e7e7a904dd38127234dffc229dd82
Jae-young Hwang <j-zero.hwang@samsung.com>
2018-08-24 10:45:52 +0900

    bluetooth: split setting btmgmt from booting-done
    Change-Id: I56a66818040e6b61ea55b544ab4bfc07209e92e6
    Signed-off-by: Jae-young Hwang <j-zero.hwang@samsung.com>

---
+++ scripts/set-btmgmt.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+kernel_release=`uname -r`
+kernel_version=`expr "$kernel_release" : '\([0-9].[0-9]\+\)'`
+
+# Bluetooth
+sleep 1
+lmp_version=`hciconfig -a | grep LMP | awk '{print $3}'`
+
+# Write Secure Connections Host Support Command
+if [ $kernel_version \> 3.14 ] && [ $lmp_version \> 4.1 ]; then
+	btmgmt sc off 1>/dev/null
+fi
